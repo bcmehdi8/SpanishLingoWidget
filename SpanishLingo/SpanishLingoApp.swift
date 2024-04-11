e610bc88085b6557cec5c93b7fdb06ea87e41c8f
Mehdi <mehdiacer2@gmail.com>
2024-04-11 23:02:11 +0000

    Initial Commit

---
+++ SpanishLingo/SpanishLingoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpanishLingoApp.swift
+//  SpanishLingo
+//
+//  Created by Mehdi on 11/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct SpanishLingoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
